320e16100cd273d293835b649a3b0e3ab29ea836
Xuezheng Wang <xzzwang@icloud.com>
2020-11-06 20:41:21 -0800

    Sticky navbar

--- front-end/components/commonUI.js
+++ front-end/components/commonUI.js
@@ -7,16 +7,16 @@ export class GaryNavbar extends React.Component {
 
     render() {
         return (
-            <Navbar bg="dark" variant='dark' fixed="top">
+            <Navbar bg="dark" variant='dark' sticky="top">
                 <Link href='/' >
                     <Navbar.Brand className='link'>
                         <img src="/logo/PCLogo-Color.svg" alt="Logo of gary planner" className="navbarLogo" />
                         Gary Planner
                     </Navbar.Brand>
                 </Link>
-                <div className='navbarItem'>
+                <Navbar.Text className='navbarItem'>
                     {this.props.children}
-                </div>
+                </Navbar.Text>
             </Navbar>
         )
     }
