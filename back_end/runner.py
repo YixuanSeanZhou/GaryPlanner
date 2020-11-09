6f1291abd126e37f517a160897c3504ead1d3e81
Yixuan Zhou <thomaszhou2333@gmail.com>
2020-11-08 23:49:33 -0800

    trying..

--- back_end/runner.py
+++ back_end/runner.py
@@ -5,12 +5,34 @@ import nltk
 nltk.download('wordnet')
 nltk.download('punkt')
 
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+
+def set_chrome_options() -> None:
+    """Sets chrome options for Selenium.
+    Chrome options for headless browser is enabled.
+    """
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+    return chrome_options
+
+
 @cli.command("create_db")
 def create_db():
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
-    test()
+    # db_DropEverything(db)
+    # db.create_all()
+    # db.session.commit()
+    # test()
+    chrome_options = set_chrome_options()
+    driver = webdriver.Chrome(options=chrome_options)
+    # Do stuff with your driver
+    driver.close()
 
 
 def test(): 
@@ -20,3 +42,4 @@ def test():
 
 if __name__ == "__main__":
     cli()
+
