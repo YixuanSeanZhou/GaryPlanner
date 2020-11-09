c5342d377ff063d7d60b8e296860f424bba3a3cd
Xuezheng Wang <xzzwang@icloud.com>
2020-11-08 22:40:19 -0800

    Installed auth0 package, enabled client-side login

--- front-end/pages/_app.js
+++ front-end/pages/_app.js
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Auth0Provider } from "@auth0/auth0-react";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Auth0Provider
+      domain="garyplanner.us.auth0.com"
+      clientId="nMnVSzU4rg1q3Ipe7mnf4fI7HblUWxvW"
+      redirectUri={"http://localhost:3000/login"}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  )
 }
 
 export default MyApp
--- front-end/pages/login.js
+++ front-end/pages/login.js
@@ -1,5 +1,7 @@
+import React from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import { useAuth0 } from "@auth0/auth0-react";
 
 // Components
 import { GaryNavbar } from '../components/commonUI'
@@ -9,6 +11,7 @@ import { Button, Form, Navbar } from 'react-bootstrap'
 import styles from "../styles/Login.module.css"
 
 export default function Login() {
+
     return (
         <>
             <Head>
@@ -17,24 +20,67 @@ export default function Login() {
 
             <GaryNavbar>
                 <Navbar.Text>
-                   Login 
+                    Authentication 
                 </Navbar.Text>
             </GaryNavbar>
 
             <div className={styles.main}>
-                <Form>
-                    <Form.Group controlId='loginName' >
-                        <Form.Label>User Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter User Name" />
-                    </Form.Group>
-                    <Form.Group controlId='loginPassword' >
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <br />
-                    <Button variant="primary" type='submit'>Submit</Button>
-                </Form>
+                <UserInfoDisplay />
+                <GaryLoginButton />
+                <GaryLogoutButton />
             </div>
         </>
-    )
+    );
 }
+
+function UserInfoDisplay() {
+    const { user, isAuthenticated, isLoading } = useAuth0();
+
+    if (isAuthenticated) {
+        return (
+            <>
+                <h2>
+                    Logged in! Congrats!
+                </h2>
+                <h5>
+                    User Name: {user.name}
+                </h5>
+                <h5>
+                    User Email: {user.email}
+                </h5>
+            </>        
+        );
+    } else {
+        return (
+            <h2>User not logged in!</h2>
+        );
+    }
+}
+
+
+function GaryLoginButton(props) {
+    const { loginWithRedirect } = useAuth0();
+
+    return(
+        <Button 
+            variant="primary" 
+            onClick={() => loginWithRedirect()} 
+            className={styles.buttons}
+        >
+            Login
+        </Button>    
+    );
+
+}
+
+
+const GaryLogoutButton = () => {
+    const { logout } = useAuth0();
+  
+    return (
+      <Button onClick={() => logout({ returnTo: "http://localhost:3000/login"})} className={styles.buttons}>
+        Log Out
+      </Button>
+    );
+  };
+  
