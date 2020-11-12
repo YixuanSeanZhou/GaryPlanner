b250300e54d98561ac7d07b969c8dfea47d909d7
David Song <d5song@ucsd.edu>
2020-11-11 19:55:40 -0800

    made fixes to 4yrplan and requirements

--- back_end/src/models/four_year_plan.py
+++ back_end/src/models/four_year_plan.py
@@ -4,17 +4,25 @@ from typing import List
 
 from ..setup import db
 
+from .user import User
+from .all_classes import AllClasses
+from .class_schedule import ClassSchedule
+
+
 class FourYearPlan(db.Model):
-    tableName = "FourYearPlan"
+    __tableName__ = "FourYearPlan"
 
     # TODO: Check nullable and foreign key class names
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-    class_id = db.Column(db.Integer, db.ForeignKey('all_classes.id'), nullable = False)
-    class_schedule_id = db.Column(db.Integer, db.ForeignKey('class_schedule.id'), nullable = False)
-    quarter_taken = db.Column(db.String(32), nullable = False)
-    grade = db.Column(db.String(8), nullable = True) 
-    locked = db.Column(db.Boolean, nullable = True) 
+    User.id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+    AllClasses.id = db.Column(db.Integer,
+                              db.ForeignKey('AllClasses.id'), nullable=False)
+    ClassSchedule.id = db.Column(db.Integer,
+                                 db.ForeignKey('ClassSchedule.id'),
+                                 nullable=False)
+    quarter_taken = db.Column(db.String(32), nullable=False)
+    grade = db.Column(db.String(8), nullable=True)
+    locked = db.Column(db.Boolean, nullable=True)
 
     def __init__(self, **kwargs):
         super(FourYearPlan, self).__init__(**kwargs)
@@ -22,9 +30,9 @@ class FourYearPlan(db.Model):
     def to_json(self):
         ret = {}
         ret['id'] = self.id
-        ret['user_id'] = self.user_id
-        ret['class_id'] = self.class_id
-        ret['class_schedule_id'] = self.class_schedule_id
+        ret['User.id'] = self.User.id
+        ret['AllClasses.id'] = self.AllClasses.id
+        ret['ClassSchedule.id'] = self.ClassSchedule.id
         ret['quarter_taken'] = self.quarter_taken
         ret['grade'] = self.grade
         ret['locked'] = self.locked
@@ -45,71 +53,74 @@ class FourYearPlan(db.Model):
             self.grade = grade
         if locked:
             self.locked = locked
-        return True   
+        self.save()
+        return True
 
     def save(self):
         db.session.commit()
 
     @staticmethod
     def create_entry(user_id: int, class_id: int,
-                    class_schedule_id: int, quarter_taken: str,
-                    grade: str, locked: bool) -> bool:
+                     class_schedule_id: int,
+                     quarter_taken: str,
+                     grade: str, locked: bool) -> bool:
         # if it's an identical user, class, and quarter, return false
-        if FourYearPlan.get_unique_entry(user_id=user_id, 
-                class_id=class_id, quarter_taken=quarter_taken):
+        if FourYearPlan.get_unique_entry(user_id=user_id,
+                                         class_id=class_id,
+                                         quarter_taken=quarter_taken):
             return False
-        four_year_plan = FourYearPlan(user_id=user_id, class_id=class_id, 
-                class_schedule_id=class_schedule_id, quarter_taken=quarter_taken, 
-                grade=grade, locked=locked)
+        four_year_plan = FourYearPlan(user_id=user_id, class_id=class_id,
+                                      class_schedule_id=class_schedule_id,
+                                      quarter_taken=quarter_taken,
+                                      grade=grade, locked=locked)
         db.session.add(four_year_plan)
         four_year_plan.save()
         return True
 
-    #returns entire table
+    # returns entire table
     @staticmethod
     def get_entries() -> List[FourYearPlan]:
         entries = FourYearPlan.query.all()
-        entries = list(map(lambda x: x.to_json(), entries))
         return entries
 
-    #Returns all entries composing the plan for one user
+    # Returns all entries composing the plan for one user
     @staticmethod
     def get_plan_by_user(user_id: int) -> List[FourYearPlan]:
-        plan = FourYearPlan.query.filter_by(user_id=user_id)
-        plan = list(map(lambda x: x.to_jason(), plan))
+        plan = FourYearPlan.query.filter_by(user_id=user_id).all()
         return plan
 
-    #Returns all locked entries in a four year plan
+    # Returns all locked entries in a four year plan
     @staticmethod
-    def get_plan_by_user(user_id: int, locked: bool) -> List[FourYearPlan]:
-        plan = FourYearPlan.query.filter_by(user_id=user_id, locked=locked)
-        plan = list(map(lambda x: x.to_jason(), plan))
+    def get_locked_entries_by_user(user_id: int,
+                                   locked: bool) -> List[FourYearPlan]:
+        plan = FourYearPlan.query.filter_by(user_id=user_id,
+                                            locked=locked).all()
         return plan
 
-    #returns a specific unique entry by the surrogate key
+    # returns a specific unique entry by the surrogate key
     @staticmethod
     def get_entry_by_id(plan_id: int) -> FourYearPlan:
         return FourYearPlan.query.filter_by(id=plan_id).first()
 
-    #Returns specific unique entry by user, class, and quarter
+    # Returns specific unique entry by user, class, and quarter
     @staticmethod
-    def get_unique_entry(user_id: int, class_id: int, quarter_taken: string) -> FourYearPlan:
-        return FourYearPlan.query.filter_by(user_id=user_id, 
-                class_id=class_id, quarter_taken=quarter_taken).first()
+    def get_unique_entry(user_id: int, class_id: int,
+                         quarter_taken: str) -> FourYearPlan:
+        return FourYearPlan.query.filter_by(
+            user_id=user_id,
+            class_id=class_id,
+            quarter_taken=quarter_taken).first()
 
-
-    # TODO: Is there an equivalent method to implement for four year plan?
-
-    # @staticmethod
-    # def update_profile(user_id: int, first_name: str = None,
-    #                    last_name: str = None,
-    #                    user_name: str = None,
-    #                    intended_grad_quarter: str = None,
-    #                    college: str = None, major: str = None,
-    #                    minor: str = None) -> bool:
-    #     # TODO: Maybe we want to use **kwargs, but maybe not...
-    #     usr = User.get_user_by_id(user_id=user_id)
-    #     return usr.update_attr(first_name=first_name, user_name=user_name,
-    #                            last_name=last_name, college=college,
-    #                            intended_grad_quarter=intended_grad_quarter,
-    #                            major=major, minor=minor)
+    @staticmethod
+    def update_entry(id: int, user_id: int = None,
+                     class_id: int = None,
+                     class_schedule_id: int = None,
+                     quarter_taken: str = None,
+                     grade: str = None,
+                     locked: bool = None) -> bool:
+        # TODO: Maybe we want to use **kwargs, but maybe not...
+        entry = FourYearPlan.get_entry_by_id(id=id)
+        return entry.update_attr(user_id=user_id, class_id=class_id,
+                                 class_schedule_id=class_schedule_id,
+                                 quarter_taken=quarter_taken,
+                                 grade=grade, locked=locked)
--- back_end/src/models/requirements.py
+++ back_end/src/models/requirements.py
@@ -4,12 +4,18 @@ from typing import List
 
 from ..setup import db
 
+from .majors import Majors
+from .minors import Minors
+
+
 class Requirements(db.Model):
-    tableName = "Requirements"
+    __tableName__ = "Requirements"
 
     id = db.Column(db.Integer, primary_key=True)
-    major_id = db.Column(db.Integer, db.ForeignKey('majors.id'), nullable=True)
-    minor_id = db.Column(db.Integer, db.ForeignKey('minors.id'), nullable=True)
+    Majors.id = db.Column(db.Integer, db.ForeignKey('Majors.id'),
+                          nullable=True)
+    Minors.id = db.Column(db.Integer, db.ForeignKey('Minors.id'),
+                          nullable=True)
     class_id = db.Column(db.String, nullable=False)
     category = db.Column(db.String, nullable=True)
     subcategory = db.Column(db.String, nullable=True)
@@ -20,14 +26,15 @@ class Requirements(db.Model):
     def to_json(self):
         ret = {}
         ret['id'] = self.id
-        ret['major_id'] = self.major_id
-        ret['minor_id'] = self.minor_id
+        ret['Majors.id'] = self.Majors.id
+        ret['Minors.id'] = self.Minors.id
         ret['class_id'] = self.class_id
         ret['category'] = self.category
         ret['subcategory'] = self.subcategory
+        return ret
 
-    def update_attr(self, major_id: int, minor_id: int, class_id: string, 
-                    category: string, subcategory: string) -> bool:
+    def update_attr(self, major_id: int, minor_id: int, class_id: str,
+                    category: str, subcategory: str) -> bool:
         if major_id:
             self.major_id = major_id
         if minor_id:
@@ -38,18 +45,19 @@ class Requirements(db.Model):
             self.category = category
         if subcategory:
             self.subcategory = subcategory
+        self.save()
         return True
 
     def save(self):
         db.session.commit()
 
     @staticmethod
-    def create_requirement(major_id: int, minor_id: int, class_id: string, 
-                    category: string, subcategory: string) -> bool:
+    def create_requirement(major_id: int, minor_id: int, class_id: str,
+                           category: str, subcategory: str) -> bool:
         # check for identical entries to return false
-        requirement = Requirements(major_id=major_id, minor_id=minor_id, 
-                                class_id=class_id, category=category, 
-                                subcategory=subcategory)
+        requirement = Requirements(major_id=major_id, minor_id=minor_id,
+                                   class_id=class_id, category=category,
+                                   subcategory=subcategory)
         db.session.add(requirement)
         requirement.save()
         return True
@@ -58,32 +66,34 @@ class Requirements(db.Model):
     @staticmethod
     def get_requirements() -> List[Requirements]:
         requirements = Requirements.query.all()
-        requirements = list(map(lambda x: x.to_json(), requirements))
         return requirements
 
     # get requirements by major, category, and subcategory, if indicated
     @staticmethod
-    def get_requirements_by_major(major_id: major_id, category: category, subcategory: subcategory) -> List[Requirements]:
+    def get_requirements_by_major(
+                major_id: Majors.id, category: category,
+                subcategory: subcategory) -> List[Requirements]:
         if category:
             if subcategory:
                 # major, category, and subcategory provided
-                requirements = Requirements.query.filter_by(major_id=major_id, category=category, subcategory=subcategory)
+                requirements = Requirements.query.filter_by(
+                        major_id=major_id,
+                        category=category,
+                        subcategory=subcategory).all()
             else:
                 # only major and category provided
-                requirements = Requirements.query.filter_by(major_id=major_id, category=category)
+                requirements = Requirements.query.filter_by(
+                        major_id=major_id,
+                        category=category).all()
         else:
             # only major provided
-            requirements = Requirements.query.filter_by(major_id=major_id)
-        requirements = list(map(lambda x: x.to_jason(), requirements))
+            requirements = Requirements.query.filter_by(
+                    major_id=major_id).all()
         return requirements
 
     # get requirements by minor
     @staticmethod
-    def get_requirements_by_minor(minor_id: minor_id) -> List[Requirements]:
-        requirements = Requirements.query.filter_by(minor_id=minor_id)
+    def get_requirements_by_minor(minor_id: Minors.id) -> List[Requirements]:
+        requirements = Requirements.query.filter_by(minor_id=minor_id).all()
         requirements = list(map(lambda x: x.to_jason(), requirements))
         return requirements
-
-    # Any more getters or methods required?
-
-    
