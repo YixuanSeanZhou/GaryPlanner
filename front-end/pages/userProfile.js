e5697d5671c31860d0545ebaac271e7a64fc798c
Hann4266 <ziz359@ucsd.edu>
2020-11-12 21:47:00 -0800

    front-profilepage

--- front-end/pages/userProfile.js
+++ front-end/pages/userProfile.js
@@ -1,10 +1,10 @@
 // React and Next
 import Head from 'next/head'
-
+import Link from 'next/link'
 // Components
 import { GaryNavbar } from '../components/commonUI'
-import { Navbar } from 'react-bootstrap';
-
+import { Button, Form, Navbar } from 'react-bootstrap';
+import styles from "../styles/UserProfile.module.css"
 export default function UserProfile() {
     return (
         <>
@@ -17,6 +17,35 @@ export default function UserProfile() {
                     User Profile
                 </Navbar.Text>
             </GaryNavbar>
+            
+            <div className={styles.main}>
+                <img src="public/logo/PCLogo-Color.svg"  width="256" height="256" />
+                <section >
+                    <div >
+                        <h2>LoginName</h2>
+                    </div>
+                    Major
+                    <p>xxx</p>
+                    Minor
+                    <p>xxx</p>
+                    Email
+                    <p>xxx@xxx</p>
+                    Bio
+                    <p>xxxxxxxx</p>
+                    <br />
+                    <div >
+                    <Link href="/setting">
+                        <Button variant="primary">Edit Profile</Button>
+                    </Link>
+                    <Link href="/ChangePass">
+                        <Button variant="primary">Change Password</Button>
+                    </Link>
+                    </div>
+                   
+                    
+                
+            </section>
+            </div>
         </>
-    )
-}
+    );
+};
