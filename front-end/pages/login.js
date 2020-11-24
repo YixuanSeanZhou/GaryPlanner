8410b2e8d45c41b6b7e063e7965b0d766a9c223a
Xuezheng Wang <xzzwang@icloud.com>
2020-11-23 17:47:06 -0800

    No more white bars

--- front-end/pages/login.js
+++ front-end/pages/login.js
@@ -6,8 +6,9 @@ import { withRouter } from 'next/router';
 import Image from 'next/image';
 
 // Components
-import { Form, Button, Navbar } from 'react-bootstrap';
-import { GaryNavbar } from '../components/commonUI';
+import { Form, Button, Navbar, Spinner } from 'react-bootstrap';
+import { GaryNavbar, ParticleEffect } from '../components/commonUI';
+import LoadingOverlay from 'react-loading-overlay';
 import Particles from 'react-particles-js';
 
 // Styles
@@ -18,14 +19,20 @@ class Login extends React.Component {
 		super(props);
 
 		this.state = {
-			email: "",
-			pwd: "",
+			formData: {
+				email: "",
+				pwd: "",	
+			},
+			isLoading: false,
 		}
 	}
 
 
 	// Invoked when the user hit click
 	handleClick = (e) => {
+		// First, enable loading animation
+		this.setState({isLoading: true})
+
 		console.log("POSTing this data to server:", JSON.stringify(this.state));
 
 		// Options for the fetch request
@@ -36,7 +43,7 @@ class Login extends React.Component {
 				'Content-Type': 'application/json',
 			},
 			credentials: 'include', // Everything account related
-			body: JSON.stringify(this.state),
+			body: JSON.stringify(this.state.formData),
 		};
 
 		fetch(requestUrl, options)
@@ -51,11 +58,15 @@ class Login extends React.Component {
 			} else if (response.status == 300) {
 				// User Already Existed!
 				// TODO: Prompt
-			}	
+			} else {
+				// Server issue
+			}
+			this.setState({isLoading: false});
 			console.log('Success:', data); // TODO: Remove for deployment
 		})
 		.catch((error) => {
 			console.error('Error:', error);
+			this.setState({isLoading: false});
 		});
 	}
 
@@ -63,128 +74,106 @@ class Login extends React.Component {
 	// Invoked everytime the value in the two textboxes changes
 	handleChange = (e) => {
 		this.setState({
-			[e.target.id]: e.target.value
+			formData: {
+				[e.target.id]: e.target.value
+			}
 		});
 	}
 
 	render() {
 		return (
-			<>
+			<LoadingOverlay
+				active={this.state.isLoading}
+				spinner
+				text="Please wait"
+			>
+
 				<Head>
 					<title>Log in</title>
 				</Head>
 
-				<GaryNavbar>
-					<Navbar.Text>Log in</Navbar.Text>
-				</GaryNavbar>
-	
-				<div className={styles.outer}>
-					<Particles
-						params={{
-							"particles": {
-							"number": {
-							"value": 90,
-							"density": {
-							"enable": true,
-							"value_area": 2000
-							}
-							},
-							"color": {
-							"value": "#ffffff"
-							},
-							"size": {
-							"value": 2.5
-							}
-						},
-							"interactivity": {
-							"events": {
-							"onhover": {
-							"enable": true,
-							"mode": "repulse"
-							}
-							}
-							}
-						}}
-					/>
-					<div className={styles.middle} style={{
-						position: "absolute",
-						top: "20%",
-						left: 0,
-						width: "100%",
-						height: "absolute"
-						
-					}}>
-						<div className={styles.login}>
-							<Form.Group style={{ display: 'flex', alignItems: 'center' }}>
-								<a href="/intro">
-									<Image
-										id="loginlogo"
-										src="/logo/PCLogo-Color.svg"
-										height="70"
-										width="49"
-										alt="logo"
-										className=""
-									/>
-								</a>
-								<h3 className="mt-3 ml-1" style={{ paddingLeft: '10px' }}>
-									Gary <br /> Planner
-								</h3>
-							</Form.Group>
 
-							<h3>Log in</h3>
-							<Form>
-								{/* Here are the two credentials */}
-								<Form.Group controlId="email">
-									<Form.Label>Email</Form.Label>
-									<Form.Control 
-										type="text"
-										value={this.state.email}
-										onChange={this.handleChange}
+				<div className={styles.outer}>
+
+					<GaryNavbar>
+						<Navbar.Text>Log in</Navbar.Text>
+					</GaryNavbar>
+
+					<ParticleEffect />
+	
+					{/* Start of the login component */}
+					<div className={styles.login}>
+						<Form.Group style={{ display: 'flex', alignItems: 'center' }}>
+							<a href="/intro">
+								<Image
+									id="loginlogo"
+									src="/logo/PCLogo-Color.svg"
+									height="70"
+									width="49"
+									alt="logo"
+									className=""
+								/>
+							</a>
+							<h3 className="mt-3 ml-1" style={{ paddingLeft: '10px' }}>
+								Gary <br /> Planner
+							</h3>
+						</Form.Group>
+
+						<h3>Log in</h3>
+						<Form>
+							{/* Here are the two credentials */}
+							<Form.Group controlId="email">
+								<Form.Label>Email</Form.Label>
+								<Form.Control 
+									type="text"
+									value={this.state.formData.email}
+									onChange={this.handleChange}
+								/>
+							</Form.Group>
+		
+							<Form.Group controlId="pwd">
+								<Form.Label >Passowrd</Form.Label>
+								<Form.Control 
+									type="password"
+									value={this.state.formData.pwd}
+									onChange={this.handleChange}
+								/>
+							</Form.Group>
+		
+							<Form.Group>
+								<Form.Label>
+									<Form.Check
+										type="checkbox"
+										name="remember"
+										label="Remember me"
 									/>
-								</Form.Group>
-			
-								<Form.Group controlId="pwd">
-									<Form.Label >Passowrd</Form.Label>
-									<Form.Control 
-										type="password"
-										value={this.state.pwd}
-										onChange={this.handleChange}
-									/>
-								</Form.Group>
-			
-								<Form.Group>
-									<Form.Label>
-										<Form.Check
-											type="checkbox"
-											name="remember"
-											label="Remember me"
-										/>
-									</Form.Label>
-								</Form.Group>
-			
-								<Form.Group>
-									<Form.Text style={{ fontSize: '.85rem' }}>
-										New to GaryPlanner?{' '}
-										<Link href="/signup" >
-											<a style={{ color: '#0067b8' }}>
-												Create an account
-											</a>
-										</Link>
-									</Form.Text>
-								</Form.Group>
-								<div style={{ textAlign: 'right' }}>
-									<Button
-										value="submit"
-										onClick={this.handleClick}
-									>
-										Login
-									</Button>
-								</div>
-							</Form>
-						</div>
+								</Form.Label>
+							</Form.Group>
+		
+							<Form.Group>
+								<Form.Text style={{ fontSize: '.85rem' }}>
+									New to GaryPlanner?{' '}
+									<Link href="/signup" >
+										<a style={{ color: '#0067b8' }}>
+											Create an account
+										</a>
+									</Link>
+								</Form.Text>
+							</Form.Group>
+							<div style={{ textAlign: 'right' }}>
+								<Button
+									value="submit"
+									onClick={this.handleClick}
+								>
+									Login
+								</Button>
+							</div>
+						</Form>
 					</div>
+
 				</div>
-			</>
+
+			</LoadingOverlay>
 		)
 	}
 }
