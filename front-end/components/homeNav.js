44342a3054bec3b940f07bfd22bcdf06c6ae2656
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2020-11-28 14:29:48 -0800

    home & intro page

--- front-end/components/homeNav.js
+++ front-end/components/homeNav.js
@@ -23,28 +23,24 @@ export default class HomeNav extends React.Component {
 						</Navbar.Brand>
 					</Link>
 
-
-
 					<Nav className="ml-auto">
-						<Navbar.Text>
-							Welcome,
-						</Navbar.Text>
+						<Navbar.Text>Welcome,</Navbar.Text>
 
-							<NavDropdown className="ml-auto" title="Yixuan" alignRight>
-								<NavDropdown.Item href="/userProfile">
-									View Profile
-								</NavDropdown.Item>
-								<NavDropdown.Item href="/friends">Friends</NavDropdown.Item>
-								<NavDropdown.Item href="changePass">
-									Change Passoword
-								</NavDropdown.Item>
-							</NavDropdown>
-				
+						<NavDropdown className="ml-auto" title="Yixuan" alignRight>
+							<NavDropdown.Item href="/userProfile">
+								Edit Profile
+							</NavDropdown.Item>
+							<NavDropdown.Item href="/friends">Friends</NavDropdown.Item>
+							<NavDropdown.Item href="changePass">
+								Change Passoword
+							</NavDropdown.Item>
+						</NavDropdown>
+
 						<Nav.Link href="/intro">
-							|  <span style={{textDecoration:"underline"}}>Log out</span>
+							| <span style={{ textDecoration: 'underline' }}>Log out</span>
 						</Nav.Link>
 					</Nav>
-				</Navbar>								
+				</Navbar>
 			</>
 		)
 	}
--- front-end/pages/home.js
+++ front-end/pages/home.js
@@ -1,117 +1,100 @@
 // React and Next
-import React from 'react'
+import React, {useState} from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
 
 // Components
 import HomeNav from '../components/homeNav'
 import { ParticleEffect } from '../components/commonUI';
 import { Navbar, Jumbotron, Button, Modal, Card } from 'react-bootstrap'
 import Request from '../components/requestPlan'
-import Link from 'next/link'
 import EvaluationsList from '../components/eval/evaluationsList';
 
 import styles from '../styles/Auth.module.css'
 
-export default function Home() {
+export default class Home extends React.Component {
 
-	const [modalShow, setModalShow] = React.useState(false);
+	constructor(props) {
+        super(props);
 
-	return (
-		<>
-			<Head>
-				<title>Home</title>
-			</Head>
+        this.state = {
+			modalShow: false,
 
-			<HomeNav>
-				<Navbar.Text>Home</Navbar.Text>
-			</HomeNav>
+            is_loading: true,
+            user_name: "Loading...",
+            email: "Loading...",
+            first_name: "Loading...",
+            last_name: "Loading...",
+            college: "Loading...",
+            intended_grad_quarter: "Loading...",
+            major: "Loading...",
+            minor: "Loading...",
+		}
+		
+	}
 
-			<ParticleEffect className={styles.particles} />
+	render() {
+		
+		return (
+			<>
+				<Head>
+					<title>Home</title>
+				</Head>
 
-			<div className={styles.outer}>
-				<div className={styles.rowContent}>
-					<div className="row">
-						<div className="offset-1 col-4 mt-5">
-							<Card>
-								<Card.Body>
-									<h4 className="mb-4">Yixuan Zhou</h4>
-									<Card.Text>User Name: Pig</Card.Text>
-									<Card.Text>Major: Computer Science</Card.Text>
-									<Card.Text>Minor: None</Card.Text>
-									<Card.Text>College: Sixth</Card.Text>
-									<Card.Text>Email: xxxx007@ucsd.edu</Card.Text>
-								</Card.Body>
+				<HomeNav>
+					<Navbar.Text>Home</Navbar.Text>
+				</HomeNav>
 
-							</Card>
-						</div>
-						<div className="offset-2 col-4 mt-5">
-							<div>
-								<Link href="/friends">
-									<Button  size="lg" variant="warning" style={{backgroundColor: "#ffcc5c"}} block id="home-btn">
-										<span>Checkout Friends' Schedules</span>
-									</Button>
-								</Link>
+				<ParticleEffect className={styles.particles} />
+
+				<div className={styles.outer}>
+					<div className={styles.rowContent}>
+						<div className="row">
+							<div className="offset-1 col-4 mt-5">
+								<Card>
+									<Card.Body>
+										<h3 className="mb-4">Yixuan Zhou</h3>
+										<Card.Text>User Name: Pig</Card.Text>
+										<Card.Text>Major: Computer Science</Card.Text>
+										<Card.Text>Minor: None</Card.Text>
+										<Card.Text>College: Sixth</Card.Text>
+										<Card.Text>Email: xxxx007@ucsd.edu</Card.Text>
+									</Card.Body>
+
+								</Card>
 							</div>
-							
-							<div style={{marginTop: "180px"}}>
-								<Link href="fourYearPlan">
-									<Button size="lg" variant="info" style={{backgroundColor: "#ff6f69"}} block id="home-btn">
-										<span>View Four Year Plan</span>
+							<div className="offset-2 col-4 mt-5">
+								
+								<div >
+									<Button size="lg" variant="info" style={{backgroundColor: "#88d8b0"}} block id="home-btn" onClick={() => this.setState({modalShow: true})}>
+										<span>New Four Year Plan</span>
 									</Button>										
-								</Link>
+								</div>
+								
+								<div style={{marginTop: "180px"}}>
+									<Link href="/fourYearPlan">
+										<Button size="lg" variant="warning" style={{backgroundColor: "#ffcc5c"}} block id="home-btn">
+											<span>View Four Year Plan</span>
+										</Button>										
+									</Link>
+								</div>
+								
+								<div style={{marginTop: "180px"}}>
+									<Link href="/friends">
+										<Button size="lg" variant="danger" style={{backgroundColor: "#ff6f69"}} block id="home-btn">
+											<span>Checkout Friends' Schedules</span>
+										</Button>
+									</Link>
+								</div>
 							</div>
-
 						</div>
 					</div>
-
 				</div>
-			</div>
-{/* 
-			<div className="intro">
-				<div className="content">
-					<div className="col-6">
-						<h3>Yixuan Zhou</h3>
-						<div className="container">
-							<div className="row">
-								<h6>Major: Computer Science</h6>
-							</div>
-							<div className="row">
-								<h6>Minor: P.I.G.</h6>
-							</div>
-							<div className="row">
-								<h6>College: Sixth</h6>
-							</div>
-						</div>
-						<div className="mt-4">
-							<h4>Four Year Plan</h4>
-							<Button
-								variant="warning"
-								href="/fourYearPlan"
-								className="mr-4">
-								View
-							</Button>
-							<Button variant="outline-warning" onClick={() => setModalShow(true)}>
-								Request
-							</Button>
-						</div>
-					</div>
 
-				</div>
-			</div>
-
-			<div className="content">
-				<h1 className="text-center mt-5">Checkout classes you want to take</h1>
-				<div className="container">
-					<SearchBar /> 
-					<EvaluationsList evaluations={evaluations} />
-
-				</div>
-			</div>
-
-			
-*/}
-			<Request show={modalShow} onHide={() => setModalShow(false)} />
-		</>
-	)
+				<Request show={this.state.modalShow} onHide={() => this.setState({modalShow: false})} />
+			</>
+		)
+	}
+	
 
 }
