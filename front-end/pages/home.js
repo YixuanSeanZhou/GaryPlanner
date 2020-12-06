bc550c3caf98947ee9b9357ee3b2acdbeba06b03
Xuezheng Wang <xzzwang@icloud.com>
2020-12-06 12:21:32 -0800

    Fix homePage layout

--- front-end/pages/home.js
+++ front-end/pages/home.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 // Components
 import { ParticleEffect, GaryNavbar } from '../components/commonUI';
-import { Navbar, Jumbotron, Button, Modal, Card } from 'react-bootstrap'
+import { Navbar, Jumbotron, Button, Modal, Card, Container, Row, Col } from 'react-bootstrap'
 import Request from '../components/requestPlan'
 
 // Style sheets
@@ -59,74 +59,70 @@ export default class Home extends React.Component {
 
 				<ParticleEffect className={authStyles.particles} />
 
-				<div className={authStyles.outer}>
-					<div className={authStyles.rowContent}>
-						<div className="row">
-							<div className="offset-1 col-4 mt-5">
-								<Card>
-									<Card.Body>
-										<h3 className="mb-4">
-											{userProfile.first_name} {userProfile.last_name}
-										</h3>
-										<Card.Text>
-											User Name: {userProfile.user_name}
-										</Card.Text>
-										<Card.Text>Major: {userProfile.major}</Card.Text>
-										<Card.Text>Minor: {userProfile.minor}</Card.Text>
-										<Card.Text>
-											College: {userProfile.college}
-										</Card.Text>
-										<Card.Text>Email: {userProfile.email}</Card.Text>
-										<Card.Text>
-											Graduation: {userProfile.intended_grad_quarter}
-										</Card.Text>
-									</Card.Body>
-								</Card>
-							</div>
-							<div className="offset-2 col-4 mt-5">
-								<div>
+				<div className={styles.outer}>
+					<Container className={styles.content}>
+						<Row>
+							<Col>
+								<div className={styles.profile}>
+									<h3 className="mb-4">
+										{userProfile.first_name} {userProfile.last_name}
+									</h3>
+									<div>
+										User Name: {userProfile.user_name}
+									</div>
+									<div>Major: {userProfile.major}</div>
+									<div>Minor: {userProfile.minor}</div>
+									<div>
+										College: {userProfile.college}
+									</div>
+									<div>Email: {userProfile.email}</div>
+									<div>
+										Graduation: {userProfile.intended_grad_quarter}
+									</div>
+								</div>
+							</Col>
+
+							<Col>
+								<Row>
+									{/* Button one */}
 									<Button
-										size="lg"
-										variant="info"
 										style={{ backgroundColor: '#88d8b0' }}
 										block
-										id="home-btn"
+										className={`${styles.button} ${styles.firstButton}`}
 										onClick={() =>
 											this.setState({ modalShow: true })
 										}
 									>
 										<span>New Four Year Plan</span>
 									</Button>
-								</div>
+								</Row>
 
-								<div style={{ marginTop: '130px' }}>
+								<Row>
 									<Link href="/fourYearPlan">
 										<Button
-											size="lg"
-											variant="warning"
 											style={{ backgroundColor: '#ffcc5c' }}
+											className={styles.button}
 											block
-											id="home-btn">
+										>
 											<span>View Four Year Plan</span>
 										</Button>
 									</Link>
-								</div>
+								</Row>
 
-								<div style={{ marginTop: '130px' }}>
+								<Row>
 									<Link href="/friends">
 										<Button
-											size="lg"
-											variant="danger"
 											style={{ backgroundColor: '#ff6f69' }}
+											className={styles.button}
 											block
-											id="home-btn">
+										>
 											<span>Checkout Friends' Schedules</span>
 										</Button>
 									</Link>
-								</div>
-							</div>
-						</div>
-					</div>
+								</Row>
+							</Col>
+						</Row>
+					</Container>
 				</div>
 
 				<Request
